98ae54ad391f336abfb23f50af6576323ee0e228
iSnakeBuzz <contacto@isnakebuzz.com>
2021-02-08 07:39:36 -0300

    Added bcrypt to register.

--- database/graphql/mutations/createUser.js
+++ database/graphql/mutations/createUser.js
@@ -1,12 +1,19 @@
 import User from '../../models/User';
+import bcrypt from 'bcryptjs';
+
+const BCRYPT_SALT = bcrypt.genSaltSync(8);
 
 export default async (parent, args, context) => {
     let userInput = args.user;
     let date = new Date();
 
-    let password =;
+    let password = userInput.password;
+
+    var hashedPassword = bcrypt.hashSync(password, salt);
 
     let userToAdd = {
+        ...userInput,
+        password: hashedPassword,
         created_at: date,
         updated_at: date
     }
