b746a794cf2f0e4f2f1166ca8cf8abe0808610db
iSnakeBuzz <contacto@isnakebuzz.com>
2021-02-08 07:54:44 -0300

    Fixed validateJwt

--- database/graphql/queries/Auth/validateJwt.js
+++ database/graphql/queries/Auth/validateJwt.js
@@ -4,14 +4,14 @@ const JWT_TOKEN = process.env.JWT;
 
 export default async (parent, args, context) => {
     let auth = context.authorization;
-    console.log('validateJwt', auth)
+    let token = auth.split('Bearer ')[1];
 
-    let verified = jwt.verify(auth, JWT_TOKEN);
+    let verified = jwt.verify(token, JWT_TOKEN);
 
     console.log("Verified:", verified)
 
     return {
-        valid: verified,
-        jwt: "hlolasd"
+        valid: true,
+        jwt: token
     }
 }
