cd6a8e95770c3bed4ed5f3b966855ae8d5f4cc78
iSnakeBuzz <contacto@isnakebuzz.com>
2021-02-12 05:58:32 -0300

    Added multiple banner options

--- utils/Modules/editor/Banner.jsx
+++ utils/Modules/editor/Banner.jsx
@@ -1,19 +1,30 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 
-const Banner = () => {
+const Banner = ({ onChange }) => {
 
     const [type, setType] = React.useState('image');
+    const [value, setValue] = React.useState();
 
     const onSelectChange = (e) => {
         setType(e.target.value);
+        setValue(undefined);
     }
+
+
+    const onInputChange = (e) => {
+        setValue(e.target.value);
+
+        if (onChange)
+            onChange(type, value);
+    }
+
 
     return (
         <Grid container spacing={2}>
             <Grid item xs={9}>
 
-                <input type="file" />
+                <input onChange={onInputChange} type={type === "image" ? "file" : "text"} placeholder={type === "image" ? "Upload file." : type === "twitch" ? "Paste a Twitch link" : "Paste a YouTube link."} />
 
             </Grid>
             <Grid item xs={3}>
